c423b52f9492e0630e52fce41452fdb7b46d6e22
rafe <raphael.lafrance@gmail.com>
2020-02-02 23:59:14 -0500

    Update directory constants to work from jupyter notebooks

--- label_babel/pylib/util.py
+++ label_babel/pylib/util.py
@@ -10,6 +10,8 @@ FLAGS = regex.VERBOSE | regex.IGNORECASE
 BATCH_SIZE = 1_000_000  # How many records to work with at a time
 
 DATA_DIR = Path('.') / 'data'
+if not DATA_DIR.exists():
+    DATA_DIR = Path('..') / 'data'
 
 
 class DotDict(dict):
